ec4db9cce2c9b118f1b5eb51d6dc142d8455d269
Lliam <lliam@iMac8.local>
2018-10-16 12:07:28 -0400

    Almost finished code

--- assignment_five.py
+++ assignment_five.py
@@ -5,12 +5,16 @@ def play():
     play = input("Shall we play a game?")
     if play == "yes" or play == "Yes":
         return True
+    elif play == "no" or play == "No":
+        print("Okay fine, I didn't want you to play anyways!")
     else:
         return False
 
 
 def draw():
-    return random.randint(1, 100)
+    number = random.randint(1, 100)
+    print(number)
+    return number
 
 
 def main():
@@ -18,18 +22,19 @@ def main():
         if play():
             total_guesses = 0
             number = draw()
-            guess = int(input("Try to guess my number?"))
-            total_guesses = total_guesses + 1
-            if guess == number:
-                print("You guessed it!")
-                print("You guessed it in", total_guesses)
-                break
-            elif guess < number:
-                print("Your guess was too low")
-            else:
-                print("Your guess was too high")
+            while True:
+                guess = int(input("Try to guess my number?"))
+                total_guesses = total_guesses + 1
+                if guess == number:
+                    print("You guessed it!")
+                    print("You guessed it in", total_guesses, "tries")
+                    break
+                elif guess < number:
+                    print("Your guess was too low")
 
-        else:
+                else:
+                    print("Your guess was too high")
+
             print("Finished")
 
 
