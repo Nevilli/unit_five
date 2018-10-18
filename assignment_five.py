bacfc8a69541eeff9e05d5aafc2ee0b022221969
Lliam <lliam@iMac8.local>
2018-10-18 14:52:19 -0400

    Finished

--- assignment_five.py
+++ assignment_five.py
@@ -1,19 +1,31 @@
+# Liam Neville
+# 10/18/18
+# This program asks the user if they want to play the guessing game, if yes, they play the game, if no, they don't.
 import random
 
 
 def play():
-    play = input("Shall we play a game?")
-    if play == "yes" or play == "Yes":
+    """
+    This function determines whether the player wants to play the game or not
+    :return: False is returned twice in case the player does not want to play
+            True is returned if the player wants to play the guessing game
+    """
+    play1 = input("Shall we play a game?")
+    if play1 == "yes" or play == "Yes":
         return True
-    elif play == "no" or play == "No":
+    elif play1 == "no" or play1 == "No":
         print("Okay fine, I didn't want you to play anyways!")
+        return False
     else:
         return False
 
 
 def draw():
+    """
+    This draws the random number for the guessing game
+    :return: number is returned so the main function can use the value
+    """
     number = random.randint(1, 100)
-    print(number)
     return number
 
 
@@ -35,7 +47,9 @@ def main():
                 else:
                     print("Your guess was too high")
 
+        else:
             print("Finished")
+            break
 
 
 main()
